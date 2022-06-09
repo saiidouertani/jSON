9db9d6b0ec3e7c5c79b929c0a5025e1473f16deb
unknown <saiidouerttani@gmail.com>
2022-06-09 21:30:55 +0100

    rest of work

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
-let header = document.querySelector("header");
-let section = document.querySelector("section");
-let request = new XMLHttpRequest();
+let header = document.querySelector('header')
+let section = document.querySelector('section')
+let request = new XMLHttpRequest()
 request.open(
-  "GET",
-  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json"
-);
-request.send();
+  'GET',
+  'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json',
+)
+request.send()
 request.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
-    let result = JSON.parse(request.responseText);
-    console.log(result.members);
-    let h1 = document.createElement("h1");
+    let result = JSON.parse(request.responseText)
+    console.log(result.members)
+    let h1 = document.createElement('h1')
 
-    h1.textContent = result.squadName;
-    let p = document.createElement("p");
+    h1.textContent = result.squadName
+    let p = document.createElement('p')
 
     p.textContent =
-      "Hometown : " + result.homeTown + " formed : " + result.formed;
-    header.appendChild(h1);
-    header.appendChild(p);
+      'Hometown : ' + result.homeTown + ' formed : ' + result.formed
+    header.appendChild(h1)
+    header.appendChild(p)
     for (i = 0; i < result.members.length; i++) {
-      let article = document.createElement("article");
-      let h2 = document.createElement("h2");
-      h2.innerText = result.members[i].name;
-      let p = document.createElement("p");
-      p.innerText = `secretIdentity:${result.members[i].secretIdentity}/n Age:${result.members[i].age}`;
-      article.appendChild(h2);
-      article.appendChild(p);
-      section.appendChild(article);
+      let article = document.createElement('article')
+      let h2 = document.createElement('h2')
+      h2.innerText = result.members[i].name
+      let p = document.createElement('p')
+      p.innerText = `secretIdentity:${result.members[i].secretIdentity} \n Age:${result.members[i].age}`
+      article.appendChild(h2)
+      article.appendChild(p)
+      let superpower = document.createElement('p')
+      superpower.innerText = 'SuperPowers'
+      article.appendChild(superpower)
+      let ul = document.createElement('ul')
+      for (j = 0; j < result.members[i].powers.length; j++) {
+        let li = document.createElement('li')
+        li.innerText = result.members[i].powers[j]
+        ul.appendChild(li)
+      }
+      article.appendChild(ul)
+      section.appendChild(article)
     }
   }
-};
+}
